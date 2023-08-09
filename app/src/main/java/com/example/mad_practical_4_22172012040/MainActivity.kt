67782c244f89dd357658bac39407af07c3223904
student <student@gnu.ac.in>
2023-08-09 08:54:07 +0530

    p4d1

---
+++ app/src/main/java/com/example/mad_practical_4_22172012040/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.mad_practical_4_22172012040
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+    val tag:Button = findViewById(R.id.button)
+
+}
