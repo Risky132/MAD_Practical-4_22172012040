53f3061bdfe847206c2e251161d75f18968ecd11
PatelShivam3730 <shivampatel19@gnu.ac.in>
2023-08-24 22:00:52 +0530

    hehehehe

--- app/src/main/java/com/example/mad_practical_4_22172012040/LoginActivity.kt
+++ app/src/main/java/com/example/mad_practical_4_22172012040/LoginActivity.kt
@@ -1,11 +1,19 @@
 package com.example.mad_practical_4_22172012040
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        val button:Button=findViewById(R.id.signup)
+        button.setOnClickListener{
+            Intent(this,RegistrationActivity::class.java).also { startActivity(it) }
+        }
+
+
     }
 }
--- app/src/main/java/com/example/mad_practical_4_22172012040/MainActivity.kt
+++ app/src/main/java/com/example/mad_practical_4_22172012040/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.mad_practical_4_22172012040
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -8,7 +9,14 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val button:Button=findViewById(R.id.login)
+        val button1:Button=findViewById(R.id.signup)
+        button.setOnClickListener{
+            Intent(this,LoginActivity::class.java).also { startActivity(it) }
+        }
+        button1.setOnClickListener{
+            Intent(this,RegistrationActivity::class.java).also { startActivity(it) }
+        }
     }
-    val tag:Button = findViewById(R.id.button)
 
 }
